fd1c29a4859b9e4d03c98b693ea5783e3165c1da
Tushar <tusharsingh.official@icloud.com>
2021-09-30 14:57:30 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Error from './components/Error';
+// import React, { useRef, useState, useEffect } from 'react';
+// import { AnimatePresence } from "framer-motion"; // cover switch with the tag
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+		<Router>
+            <Switch>
+                <Route exact path="/error">
+                    <Error></Error>
+                </Route>
+                <Route component={Error}/>
+            </Switch>    
+        </Router>
+    );
 }
 
 export default App;
